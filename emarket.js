e537a1f300b36d99aef2a43f74cc4bbaddc06ecf
bekkzod <abdumannobovbexzodbek@gmail.com>
2023-01-31 22:12:08 +0500

    created carousel image slider on home page

--- emarket.js
+++ emarket.js
@@ -1,13 +1,41 @@
-const catalog = document.getElementsById('.dropdown')
-const list = document.querySelector('.vert-nav')
+const list = document.getElementById('vert-nav');
+list.style.background = "#82AAE3";
 
-list.style.display = "none";
+// dropdown menu
+function buttonClick() {
+    document.getElementById("categDrpDwn").classList.toggle("show");
+  }
+  
+// carousel
 
-catalog.addEventListener("click", (e) => {
-    if(list.style.display == "none") {
-        list.style.display = "block"
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const slider = document.querySelector('.slider');
+const imgs = document.querySelectorAll('.slider img');
+
+let index = 0;
+let width = imgs[index].clientWidth;
+
+
+
+next.addEventListener('click', () => {
+  index++;
+
+  slider.style.transform = `translate(${-index * width }px)`;
+    if(index === imgs.length - 1) {
+      next.classList.add("disable");
+    }else {
+      prev.classList.remove("disable");
     }
-    else{
-        list.style.display == "none"
-    }
-} )
+  })
+
+  prev.addEventListener('click', () => {
+    index--;
+  
+    slider.style.transform = `translate(${-index * width }px)`;
+      if(index === 0){
+        prev.classList.add("disable");
+      }else {
+        next.classList.remove("disable");
+      }
+    })
