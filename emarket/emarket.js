66dfb88aaf0fd0e5e988c664c77b8bd3296be341
bekkzod <abdumannobovbexzodbek@gmail.com>
2023-02-02 22:41:48 +0500

    navigation bar was updated , was created login page, and footer was added

--- emarket/emarket.js
+++ emarket/emarket.js
@@ -1,5 +1,5 @@
 const list = document.getElementById('vert-nav');
-list.style.background = "#82AAE3";
+list.style.background = "#EEF2E6";
 
 // dropdown menu
 function buttonClick() {
@@ -8,34 +8,36 @@ function buttonClick() {
   
 // carousel
 
-const prev = document.getElementById('prev');
-const next = document.getElementById('next');
-const slider = document.querySelector('.slider');
-const imgs = document.querySelectorAll('.slider img');
+// const prev = document.getElementById('prev');
+// const next = document.getElementById('next');
+// const slider = document.querySelector('.slider');
+// const imgs = document.querySelectorAll('.slider img');
 
-let index = 0;
-let width = imgs[index].clientWidth;
+// let index = 0;
+// let width = imgs[index].clientWidth;
 
 
 
-next.addEventListener('click', () => {
-  index++;
+// next.addEventListener('click', () => {
+//   index++;
 
-  slider.style.transform = `translate(${-index * width }px)`;
-    if(index === imgs.length - 1) {
-      next.classList.add("disable");
-    }else {
-      prev.classList.remove("disable");
-    }
-  })
+//   slider.style.transform = `translate(${-index * width }px)`;
+//     if(index === imgs.length - 1) {
+//       next.classList.add("disable");
+//     }else {
+//       prev.classList.remove("disable");
+//     }
+//   })
 
-  prev.addEventListener('click', () => {
-    index--;
+//   prev.addEventListener('click', () => {
+//     index--;
   
-    slider.style.transform = `translate(${-index * width }px)`;
-      if(index === 0){
-        prev.classList.add("disable");
-      }else {
-        next.classList.remove("disable");
-      }
-    })
+//     slider.style.transform = `translate(${-index * width }px)`;
+//       if(index === imgs.length +1){
+//         prev.classList.add("disable");
+//       }else {
+//         next.classList.remove("disable");
+//       }
+//     })
+
+ document.getElementById('container').style.background = '#1C6758';
